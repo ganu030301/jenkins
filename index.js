79ca3f7532bf2508730e0800ebca4cfdfdbe59e1
ganu030301 <149040501+ganu030301@users.noreply.github.com>
2024-01-17 13:21:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('mama  chi mami aali');
+ res.send('respond with hello word');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
